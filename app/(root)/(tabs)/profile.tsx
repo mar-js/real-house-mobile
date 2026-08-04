a631bb3de716c0d8971f6fd675056f9059d061d3
Martin <maguera37@gmail.com>
2026-08-04 16:46:35 -0300

    feat: added Profile component with basic structure and SafeAreaView

---
+++ app/(root)/(tabs)/profile.tsx
@@ -0,0 +1,12 @@
+import { Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+export default function Profile() {
+  return (
+    <SafeAreaView>
+      <View>
+        <Text>Profile</Text>
+      </View>
+    </SafeAreaView>
+  )
+}
