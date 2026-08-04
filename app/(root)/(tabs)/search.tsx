e08518397d85c5377fc501efe23caf1f8c2b2722
Martin <maguera37@gmail.com>
2026-08-04 16:46:54 -0300

    feat: added Search component with basic structure and SafeAreaView

---
+++ app/(root)/(tabs)/search.tsx
@@ -0,0 +1,12 @@
+import { Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+export default function Search() {
+  return (
+    <SafeAreaView>
+      <View>
+        <Text>Search</Text>
+      </View>
+    </SafeAreaView>
+  )
+}
