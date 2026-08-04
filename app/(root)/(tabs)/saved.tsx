621321449b752eb522a95607aa6a0e7ce9041d04
Martin <maguera37@gmail.com>
2026-08-04 16:46:50 -0300

    feat: added Saved component with basic structure and SafeAreaView

---
+++ app/(root)/(tabs)/saved.tsx
@@ -0,0 +1,12 @@
+import { Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+export default function Saved() {
+  return (
+    <SafeAreaView>
+      <View>
+        <Text>Saved</Text>
+      </View>
+    </SafeAreaView>
+  )
+}
