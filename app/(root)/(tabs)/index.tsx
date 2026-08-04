f7019772e5795c042b89980d4e26e21ebe86b9bd
Martin <maguera37@gmail.com>
2026-08-04 16:46:31 -0300

    feat: created Home component with basic structure and SafeAreaView

---
+++ app/(root)/(tabs)/index.tsx
@@ -0,0 +1,12 @@
+import { Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+export default function Home() {
+  return (
+    <SafeAreaView>
+      <View>
+        <Text>Home</Text>
+      </View>
+    </SafeAreaView>
+  )
+}
