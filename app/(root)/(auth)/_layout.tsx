46062d30267df5f50f61a791c0f31d95734747d8
Martin <maguera37@gmail.com>
2026-08-05 14:29:08 -0300

    feat: added AuthRoutesLayout component for authentication handling

---
+++ app/(root)/(auth)/_layout.tsx
@@ -0,0 +1,12 @@
+import { useAuth } from "@clerk/expo";
+import { Redirect, Stack } from "expo-router";
+
+export default function AuthRoutesLayout() {
+  const { isLoaded, isSignedIn } = useAuth()
+
+  if (!isLoaded) return null
+
+  if (isSignedIn) return <Redirect href="/" />
+
+  return <Stack />
+}
