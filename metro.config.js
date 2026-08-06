3cec77bacf5ac320e32b7b7ad0ed681a1a9222ae
Martin <maguera37@gmail.com>
2026-08-06 17:13:20 -0300

    fix: enabled package exports to improve dependency management

--- metro.config.js
+++ metro.config.js
@@ -2,7 +2,6 @@ const { getDefaultConfig } = require('expo/metro-config');
 
 const config = getDefaultConfig(__dirname);
 
-// Desactiva los package exports si un paquete de terceros rompe el bundle
-config.resolver.unstable_enablePackageExports = false;
+config.resolver.unstable_enablePackageExports = true;
 
 module.exports = config;
