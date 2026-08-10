b4645f880e8a267cc07dded8590db78539bde90f
Martin <maguera37@gmail.com>
2026-08-10 16:36:28 -0300

    refactor: updated interface imports to use new types from '@/types/auth'

--- global/interfaces.ts
+++ global/interfaces.ts
@@ -1,61 +1,10 @@
-import type { SignInErrors, SignUpErrors } from "@clerk/expo/types";
-import type { Dispatch, SetStateAction } from "react";
-
-export interface IButtonActionSignIn {
-	text: string;
-	handlerPress: () => Promise<void>;
-	isDisabled: boolean;
-}
-
-export interface IVerifyAccountSignIn {
-	code: string;
-	setCode: Dispatch<SetStateAction<string>>;
-	isLoading: boolean;
-	errors: SignInErrors;
-	handlerVerifyCodePress: () => Promise<void>;
-	handlerRetryVerifyCodePress: () => void;
-}
-
-
-export interface IFormSignIn {
-	emailAddress: string;
-	setEmailAddress: Dispatch<SetStateAction<string>>;
-	password: string;
-	setPassword: Dispatch<SetStateAction<string>>;
-	errors: SignInErrors;
-	isLoading: boolean;
-	handlerSignInPress: () => Promise<void>;
-}
-
-export interface IResetAccount {
-	handlerCancelSignUpPress: () => void
-}
-
-export interface IFormSignUp {
-	firstName: string;
-	setFirstName: Dispatch<SetStateAction<string>>;
-	lastName: string;
-	setLastName: Dispatch<SetStateAction<string>>;
-	emailAddress: string;
-	setEmailAddress: Dispatch<SetStateAction<string>>;
-	password: string;
-	setPassword: Dispatch<SetStateAction<string>>;
-	errors: SignUpErrors;
-	isLoading: boolean;
-	handlerSignUpPress: () => Promise<void>;
-}
-
-export interface IButtonActionSignUp {
-	text: string;
-	handlerPress: () => Promise<void>;
-	isDisabled: boolean;
-}
-
-export interface IVerifyAccountSignUp {
-	code: string;
-	setCode: Dispatch<SetStateAction<string>>;
-	isLoading: boolean;
-	errors: SignUpErrors;
-	handlerVerifyCodePress: () => Promise<void>;
-	handlerRetryVerifyCodePress: () => void;
-}
+/** @deprecated Import from `@/types/auth` instead */
+export type {
+	IButtonAction as IButtonActionSignIn,
+	IButtonAction as IButtonActionSignUp,
+	IResetAccount,
+	IFormSignIn,
+	IFormSignUp,
+	IVerifyAccount as IVerifyAccountSignIn,
+	IVerifyAccount as IVerifyAccountSignUp,
+} from "@/types/auth";
