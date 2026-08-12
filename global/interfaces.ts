ac5bc2caea2741eb225f64402b5b0ab8f7329086
Martin <maguera37@gmail.com>
2026-08-12 17:11:04 -0300

    feat: added IProperty interface to define property structure

--- global/interfaces.ts
+++ global/interfaces.ts
@@ -1,10 +1,26 @@
 /** @deprecated Import from `@/types/auth` instead */
 export type {
 	IButtonAction as IButtonActionSignIn,
-	IButtonAction as IButtonActionSignUp,
-	IResetAccount,
-	IFormSignIn,
-	IFormSignUp,
-	IVerifyAccount as IVerifyAccountSignIn,
-	IVerifyAccount as IVerifyAccountSignUp,
+	IButtonAction as IButtonActionSignUp, IFormSignIn,
+	IFormSignUp, IResetAccount, IVerifyAccount as IVerifyAccountSignIn,
+	IVerifyAccount as IVerifyAccountSignUp
 } from "@/types/auth";
+
+export interface IProperty {
+	id: string
+	title: string
+	description: string
+	price: number
+	type: string
+	bedrooms: number
+	bathrooms: number
+	area_sqft: number
+	address: string
+	city: string
+	latitude: number
+	longitude: number
+	images: string[]
+	is_featured: boolean
+	is_sold: boolean
+	created_at: string
+}
