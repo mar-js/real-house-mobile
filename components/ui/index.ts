1951eb917517cb6fedcb90810063a23ae39d1fcd
Martin <maguera37@gmail.com>
2026-08-14 16:26:51 -0300

    feat: exported FilterModal from ui index

--- components/ui/index.ts
+++ components/ui/index.ts
@@ -1,3 +1,4 @@
 export { FeaturedCard } from "./FeaturedCard";
+export { FilterModal } from "./FilterModal";
 export { PlaceholderScreen } from "./PlaceholderScreen";
 export { PropertyCard } from "./PropertyCard";
