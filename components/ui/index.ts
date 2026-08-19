27bad2d78eec1d30a82c878b5c16874c670e75f2
Martin <maguera37@gmail.com>
2026-08-19 15:08:51 -0300

    feat: exported SpecItem component from ui index

--- components/ui/index.ts
+++ components/ui/index.ts
@@ -2,3 +2,4 @@ export { FeaturedCard } from "./FeaturedCard";
 export { FilterModal } from "./FilterModal";
 export { PlaceholderScreen } from "./PlaceholderScreen";
 export { PropertyCard } from "./PropertyCard";
+export { SpecItem } from "./SpecItem";
