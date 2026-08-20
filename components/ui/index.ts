fce1cb8622d29548ecf660095f904ae285a12e72
Martin <maguera37@gmail.com>
2026-08-20 16:04:18 -0300

    feat: exported MenuItem component in index file for better accessibility

--- components/ui/index.ts
+++ components/ui/index.ts
@@ -1,5 +1,6 @@
 export { FeaturedCard } from "./FeaturedCard";
 export { FilterModal } from "./FilterModal";
+export { MenuItem } from "./MenuItem";
 export { PlaceholderScreen } from "./PlaceholderScreen";
 export { PropertyCard } from "./PropertyCard";
 export { SpecItem } from "./SpecItem";
